0c2b32a6ae586f456f23cbc01382a646f5652fd0
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-23 18:11:05 +0300

    Implement EmailInvoiceStrategy

---
+++ src/Behavioral/Strategy/Strategies/Invoice/EmailInvoiceStrategy.cs
@@ -0,0 +1,17 @@
+using System;
+using Strategy.Models;
+
+namespace Strategy.Strategies.Invoice
+{
+    public class EmailInvoiceStrategy : AbstractInvoiceStrategy
+    {
+        public override void Generate(Order order)
+        {
+            var body = base.GenerateTextInvoice(order);
+
+            // TODO: code send email (SmtpClient maybe with SendGrid)
+
+            Console.WriteLine("Email sent ... ");
+        }
+    }
+}
